0eaed91f4a762a00291be461721337fc4ef2b7e9
Efe Musa Bal <64597446+efemusabal0@users.noreply.github.com>
2023-06-01 06:39:18 -0700

    Add files via upload

---
+++ day 2/data-types.js
@@ -0,0 +1,51 @@
+    let numOne = 3
+    let numTwo = 3
+
+    console.log(numOne == numTwo)
+
+    let js = 'javascript'
+    console.log (js.length)
+    console.log(js.substring(0,4))
+    let py = 'python'
+    console.log(js == py)
+
+    let nums = [1,2,3,4,5,6]
+    console.log(nums)
+
+    let car = ['mercedes','audi']
+    let cars = ['mercedes','audi']
+    console.log(car == cars)
+
+    let name ='Efe'
+    let secondName ='Musa'
+    let space = ' '
+    let fullName = name+space+secondName+space+lastName
+    console.log (fullName)
+    let userOneInfo = 'Hi'+space+'I am'+space+fullName+'.'+'I am from'+space+country+'.'+'I am'+space+age+space+'years old'+'.'
+    console.log (userOneInfo)
+    console.log ('this is a \ntest')
+    console.log ('this is a \tsecond \ntest')
+    console.log ('this\\is\\test')
+    console.log ('\'this\"is a test')
+    let testt = `${name}
+    test
+    js
+    world
+${lastName}`
+    console.log (testt)
+
+    console.log('My Website'.split(' '))
+    console.log('My Game'.split(''))
+    console.log('Car Garage'.slice())
+
+    console.log('The World Economy'.includes('World'))
+    let str = 'Hi Mike, I am Efe'
+    console.log(str)
+
+    let test3 = 'this is a index test'
+    console.log(test3.indexOf('i'))
+    console.log(test3.indexOf('this'))
+
+    console.log('Love is the best'.startsWith('Love'))
+    console.log('love is the best'.startsWith('is'))
+    console.log('Love is the best'.endsWith('best'))
