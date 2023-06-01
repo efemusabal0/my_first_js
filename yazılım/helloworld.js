72eb47f90874a0fc59a34e33bc1466b298aed0be
Efe Musa Bal <64597446+efemusabal0@users.noreply.github.com>
2023-06-01 01:13:36 -0700

    Add files via upload

---
+++ yazılım/helloworld.js
@@ -0,0 +1,9 @@
+console.log ('hello world') 
+let firstName = 'Efe',
+    lastName = 'Bal',
+    country = 'turkey',
+    age = '19',
+    schoolName = 'MSKU'
+const gravity = '9.81'
+console.log (firstName,lastName,country,age,schoolName)
+console.log (gravity)
