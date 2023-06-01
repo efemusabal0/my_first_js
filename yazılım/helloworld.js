a6b0855375d3b6c3adf0a45159e07387c3eedfca
Efe Musa Bal <64597446+efemusabal0@users.noreply.github.com>
2023-06-01 01:13:05 -0700

    Delete yazılım directory

--- yazılım/helloworld.js
+++
@@ -1,9 +0,0 @@
-console.log ('hello world') 
-let firstName = 'Efe',
-    lastName = 'Bal',
-    country = 'turkey',
-    age = '19',
-    schoolName = 'MSKU'
-const gravity = '9.81'
-console.log (firstName,lastName,country,age,schoolName)
-console.log (gravity)
--- yazılım/introduction.js
+++
@@ -1,2 +0,0 @@
-console.log ('bu yazı introduction.js den geldi')
-console.log(typeof 6)
